fa8d1d61891c08c5d5531464b4e9520a2e34bd23
Patrik Samardák <samardak1@stud.uniza.sk>
2025-12-05 16:37:22 +0100

    fix: correct display of index page on load

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});
+
 Route::controller(HomeController::class)->group(function () {
     Route::get('/', 'index')->name('home');
     Route::get('/about', 'about')->name('about');
